5097d79cc44e4b0bb8fb202335101f8bc0c6d983
Hemanuel Xafranski <hemanuel.xafranski@misterspex.de>
2022-02-20 10:11:47 +0100

    searching

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
@@ -20,7 +25,29 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value="name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		try {
+			return readStringFromURL("https://www.google.com.br/search?q=teste%20unitario&num=30");
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+//		return String.format("Hello %s!", name);
+	}
+
+	public static String readStringFromURL(String requestURL)
+	{
+		try {
+			URL url = new URL(requestURL);
+			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			httpURLConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0");
+
+			Scanner scanner = new Scanner(httpURLConnection.getInputStream(),
+					StandardCharsets.UTF_8.toString());
+			scanner.useDelimiter("\\A");
+			return scanner.hasNext() ? scanner.next() : "";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+
 	}
 
 	@GetMapping("/greeting")
